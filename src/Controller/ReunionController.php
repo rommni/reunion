64401f4a4c04b69752c006ac1df1c83a0fd88673
Julian Romain <rombatie@hotmail.fr>
2018-02-17 22:28:22 +0100

    controller et view reunion

--- src/Controller/ReunionController.php
+++ src/Controller/ReunionController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use App\Entity\Reunion;
+use App\Form\ReunionType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReunionController extends Controller
 {
@@ -13,6 +16,30 @@ class ReunionController extends Controller
      */
     public function index()
     {
-        return new Response('Welcome to your new controller!');
+        $reunions = $this->getDoctrine()->getRepository(Reunion::class)->findAll();
+        return $this->render('reunion/index.html.twig', array('reunions'=> $reunions));
+    }
+
+    /**
+     * @Route("/add", name="reunion_add")
+     */
+    public function add(Request $request)
+    {
+        $reunion = new Reunion();
+        $form = $this->createForm(ReunionType::class, $reunion);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $reunion = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($reunion);
+            $em->flush();
+
+            return $this->redirectToRoute('reunion');
+        }
+
+        return $this->render('reunion/add.html.twig', array('form'=>$form->createView()));
+
     }
 }
